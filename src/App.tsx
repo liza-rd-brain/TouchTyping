8b30e79844a1e8b4c1880422e22f1199dd159cca
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2021-06-20 01:55:54 +0300

    Added style and pseudo caret.

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,27 @@ type State = {
   index: number;
   filledString: string;
   leftedString: string;
+  currentLetter: string;
+};
+
+type SpanType = {
+  status: "filledString" | "leftedString" | "currentLetter";
 };
 
 const text: string =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, laudantium ut corrupti culpa delectus amet. Reprehenderit porro dolorum, totam pariatur ipsa dignissimos esse accusamus atque consequatur earum blanditiis impedit molestias voluptas quaerat incidunt voluptatibus perspiciatis qui debitis ratione quia. Maxime fuga autem quod! Non assumenda accusamus obcaecati, ullam ea, cum eos minus vel corporis pariatur dolor nulla, illo velit aliquam quia tempore atque saepe dolorum sapiente quasi eum hic quam laboriosam fugiat. Aperiam vero doloribus nam, quisquam temporibus molestias odit quos iste dolore facilis eligendi vitae necessitatibus recusandae eum atque tempore. Enim, ipsum! Quam quasi facilis eos laboriosam placeat molestias quibusdam, at quos, culpa obcaecati laborum illo magni ex ea voluptate repellendus nemo amet. Est et nesciunt quibusdam illum facilis voluptatem magni quos doloremque distinctio quo culpa ducimus, nihil odit similique at! Labore nobis quaerat quas similique maxime fugiat eum animi totam illum a aliquam nam eius, est assumenda error voluptatibus fugit quasi voluptatum nostrum earum, mollitia perspiciatis deleniti recusandae modi? Beatae fuga quas eos iusto quibusdam nam minus nemo ipsa autem nesciunt reiciendis impedit ut, provident cupiditate suscipit molestias enim optio consequatur saepe illo in sit! Alias delectus repellat illo voluptatibus dicta impedit ratione tempora consequuntur, sit qui reiciendis";
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis deserunt quae aliquid iusto ducimus? Consequuntur autem sequi suscipit assumenda. Ea, dolorum eum enim distinctio asperiores soluta explicabo dolore, tenetur deserunt obcaecati iste minus quo cumque recusandae fugiat! Fuga quos nisi explicabo soluta odit. Nemo repellat fugit veritatis reiciendis ipsam voluptates, laboriosam asperiores vero ea aperiam quod expedita in quidem dicta. Obcaecati dolor fuga molestias quisquam eligendi consequatur culpa iure. Itaque accusamus, facere quidem totam vitae corrupti at inventore quasi est, doloremque expedita architecto ea. Delectus libero quam, ipsum suscipit eos eius cupiditate laborum tenetur eum inventore corrupti commodi soluta impedit";
+
+const russiaString =
+  "Таким образом консультация с широким активом требуют определения и уточнения систем массового участия. Задача организации, в особенности же новая модель организационной деятельности позволяет оценить значение существенных финансовых и административных условий. Товарищи! консультация с широким активом способствует подготовки и реализации систем массового участия. Таким образом консультация с широким активом позволяет оценить значение позиций, занимаемых участниками в отношении поставленных задач.";
+const leftedString = russiaString.substr(1);
+console.log(leftedString);
+const currentLetter = russiaString.charAt(0);
 
 let state: State = {
   index: 0,
   filledString: "",
-  leftedString: text,
+  leftedString: leftedString,
+  currentLetter: currentLetter,
 };
 
 const TextArea = styled.textarea`
@@ -44,9 +56,86 @@ const TextBlock = styled.div`
   height: 300px;
   margin: 0 auto;
   padding: 20px;
+  font-family: sans-serif;
+  font-size: 18px;
+  line-height: 36px;
+  letter-spacing: 0.8px;
 `;
 
-const TextSpan = styled.span``;
+const TextSpan = styled.span<SpanType>`
+  color: ${(props) => {
+    switch (props.status) {
+      case "leftedString": {
+        return "#808080";
+      }
+      case "filledString": {
+        return "#3855c5";
+      }
+      case "currentLetter": {
+        return "white";
+      }
+      default: {
+        return "none";
+      }
+    }
+  }};
+
+  padding: ${(props) => {
+    switch (props.status) {
+      case "currentLetter": {
+        return "1px";
+      }
+      default: {
+        return "none";
+      }
+    }
+  }};
+  background-color: ${(props) => {
+    switch (props.status) {
+      case "currentLetter": {
+        return "#3855c5";
+      }
+      default: {
+        return "none";
+      }
+    }
+  }};
+
+  border: ${(props) => {
+    switch (props.status) {
+      case "currentLetter": {
+        return "1px solid #3855c5";
+      }
+      default: {
+        return "none";
+      }
+    }
+  }};
+  border-radius: 3px;
+
+  /*   &:after {
+    content: "";
+    z-index: 5;
+    position: absolute;
+
+    background-color: ${(props) => {
+    switch (props.status) {
+      case "currentLetter": {
+        return "#3855c5";
+      }
+      default: {
+        return "none";
+      }
+    }
+  }};
+  } */
+`;
+
+const HighlightBlock = styled.span`
+  width: 10px;
+  height: 10px;
+  background-color: #3855c5;
+`;
 
 const initilstate = state;
 export const App = () => {
@@ -64,9 +153,11 @@ export const App = () => {
         }}
       ></TextArea>
       <TextBlock>
-        <TextSpan>{newState.filledString}</TextSpan>
-        <TextSpan>//</TextSpan>
-        <TextSpan>{newState.leftedString}</TextSpan>
+        <TextSpan status={"filledString"}>{newState.filledString}</TextSpan>
+        <TextSpan status={"currentLetter"}>{newState.currentLetter}</TextSpan>
+        <TextSpan status={"leftedString"} id="leftedString">
+          {newState.leftedString}
+        </TextSpan>
       </TextBlock>
     </Container>
   );
@@ -93,20 +184,22 @@ const changeText = (
   setState: Function,
   enteredLetter: string
 ) => {
-  const { index, filledString, leftedString } = state;
-  const isCorrectLetter = checkLetter(leftedString, enteredLetter);
+  const { index, filledString, leftedString, currentLetter } = state;
+  const isCorrectLetter = checkLetter(currentLetter, enteredLetter);
 
   switch (isCorrectLetter) {
     case true: {
       console.log("дать следующую букву");
       const newIndex = index + 1;
+      const newCurrentLetter = leftedString.charAt(0);
       const newLeftedString = leftedString.substr(1);
       const newFilledString = filledString + enteredLetter;
-      console.log(newFilledString, newLeftedString);
+
       const newState = {
         index: newIndex,
         filledString: newFilledString,
         leftedString: newLeftedString,
+        currentLetter: newCurrentLetter,
       };
       setState(newState);
       break;
@@ -121,8 +214,8 @@ const changeText = (
   }
 };
 
-const checkLetter = (leftedString: string, enteredLetter: string) => {
-  const expectedLetter = leftedString.charAt(0);
+const checkLetter = (currentLetter: string, enteredLetter: string) => {
+  const expectedLetter = currentLetter;
   return expectedLetter === enteredLetter;
 };
 
