4f1b16395a23ab0d7dea675cafbdd8a6cff57406
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2021-06-19 21:09:39 +0300

    Added function for validating input letter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, BaseSyntheticEvent, SyntheticEvent } from "react";
 
-function App() {
+import styled from "styled-components";
+import { check } from "yargs";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+type State = {
+  index: number;
+  filledString: string;
+  leftedString: string;
+};
+
+const text: string = "Lorem ipsum dolor";
+
+let state: State = {
+  index: 0,
+  filledString: "",
+  leftedString: text,
+};
+
+const TextInput = styled.textarea`
+  border: 1px solid #000;
+  width: 800px;
+  height: 300px;
+`;
+
+const initilstate = state;
+export const App = () => {
+  const [newState, setState] = useState(initilstate);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <TextInput
+        defaultValue={getString(state)}
+        onInput={(event) => {
+          //That's how it works,meh.
+          const enteredLetter: string = (event as any).nativeEvent.data;
+          changeText(newState, setState, enteredLetter);
+        }}
+      ></TextInput>
+    </Container>
   );
-}
+};
 
-export default App;
+const getString = (state: State) => {
+  const { filledString, leftedString } = state;
+  const fullString = filledString + leftedString;
+  return fullString;
+};
+
+/**
+ * 1. We need check out is input right letter?
+ * 2. If its right we need increment index.
+ *    Then first letter from  leftedString add to filledString
+ *    and removing from leftedString simultaniously.
+ *
+ */
+
+const changeText = (
+  state: State,
+  setState: Function,
+  enteredLetter: string
+) => {
+  const { index, filledString, leftedString } = state;
+  const isCorrectLetter = checkLetter(leftedString, enteredLetter);
+
+  switch (isCorrectLetter) {
+    case true: {
+      console.log("дать следующую букву");
+      const newIndex = index + 1;
+      const newLeftedString = leftedString.substr(1);
+      const newFilledString = filledString + enteredLetter;
+      console.log(newFilledString, newLeftedString);
+      const newState = {
+        index: newIndex,
+        filledString: newFilledString,
+        leftedString: newLeftedString,
+      };
+      setState(newState);
+      break;
+    }
+    case false: {
+      console.log("Неверно");
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+};
+
+const checkLetter = (leftedString: string, enteredLetter: string) => {
+  const expectedLetter = leftedString.charAt(0);
+  return expectedLetter === enteredLetter;
+};
